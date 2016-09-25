fd402cd82b101afa6bcedc649681f029657bc793
drarntzen <drarntzen@gmail.com>
2016-09-25 11:10:24 +0800

    Initial Python Commit
    Code for the Code God! Commits for the Commit Throne!

---
+++ Python/couriers.py
@@ -0,0 +1,7 @@
+""" Python script to interact with outgoing goods database.
+
+    To be updated on an on-going basis as I learn more Python.
+    Will initially read from a .csv
+    First initial major goal is a sqlite database
+
+"""
