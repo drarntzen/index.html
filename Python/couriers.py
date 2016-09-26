a3f4e1d633f079bf4aa821ce35dc435e4c9d0d07
drarntzen <drarntzen@gmail.com>
2016-09-26 11:11:28 +0800

    Add CSV read code
    Initial CSV reading code, all ugly-like and definitely could be better.
    But it worked! \o/

--- Python/couriers.py
+++ Python/couriers.py
@@ -1,7 +1,40 @@
-""" Python script to interact with outgoing goods database.
+"""Python script to interact with outgoing goods database.
 
     To be updated on an on-going basis as I learn more Python.
     Will initially read from a .csv
     First initial major goal is a sqlite database
 
 """
+
+# import required libraries
+import csv
+
+# intialise variables used
+lineNum = 0
+csvContents = []
+# First row is a header row!
+# Column date for the CSV:
+# Column 1 (Index 0) = Date goods sent
+# Column 2 (Index 1) = Consignment reference (ex Company)
+# Column 3 (Index 2) = Transport company
+# Column 4 (Index 3) = Transport company consignment reference
+# Column 5 (Index 4) = Quantity sent (Usually 1, but not always!)
+
+with open('couriers.csv', 'rt') as csvfile:
+        lineReader = csv.reader(csvfile, delimiter=",")
+        for row in lineReader:
+            lineNum += 1
+            if lineNum == 1.0:
+                print("Skipping the header row")
+                continue
+            dateSent = row[0]
+            salesReference = row[1]
+            transportCompany = row[2]
+            transportConsignment = row[3]
+            quantitySent = row[4]
+            oneResultRow = [dateSent, salesReference, transportCompany,
+                            transportConsignment, quantitySent]
+            csvContents.append(oneResultRow)
+            print(dateSent, ",", salesReference, ",", transportCompany, ",",
+                  transportConsignment, ",", quantitySent)
+        print("Iteration done.")
